608c346c98d61e84b46ce2b8d7a054dcc230a8d8
Matt Day <mattsday@gmail.com>
2018-05-31 20:28:25 +0100

    Initial commit

---
+++ config-example.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# List of email addresses to add to the system
+EMAIL_LIST="example@example.com,example-2@example.com"
+
+# Shared org for everyone to join
+SHARED_ORG="everyone"
+
+# Ops manager details
+OPSMAN_URI="https://opsman.domain.xyz"
+OPSMAN_ADMIN_USER="admin"
+OPSMAN_ADMIN_PASS="password"
+
+# Optional but speeds things up
+
+# PKS API endpoint
+# PKS_UAA="api.pks.domain.xyz"
+
+# PAS System domain
+# PAS_SYS="sys.domain.xyz"
+
